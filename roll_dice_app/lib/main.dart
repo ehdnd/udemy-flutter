50cb4fed1d76937d6f8475412f95707ae89e8ba8
ehdnd_mac <taeungjang@naver.com>
2025-06-22 22:01:09 +0900

    section 2 fin

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:roll_dice_app/gradient_container.dart';
+
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: Center(child: Text("Hello World"))),
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(77, 153, 177, 232),
+          Color.fromARGB(255, 236, 158, 158),
+        ),
+      ),
     ),
   );
 }
