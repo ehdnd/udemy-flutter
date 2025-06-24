4452c3f91c25cfe66ad3c3c03183fa3776c68520
ehdnd_mac <taeungjang@naver.com>
2025-06-24 23:43:05 +0900

    tmp

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -13,6 +13,7 @@ class StartScreen extends StatelessWidget {
             'assets/images/quiz-logo.png',
             width: 250,
             height: 250,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(height: 80),
           const Text(
@@ -24,12 +25,13 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: const Text("Start Quiz"),
+            icon: Icon(Icons.arrow_right_alt),
+            label: const Text("Start Quiz"),
           ),
         ],
       ),
