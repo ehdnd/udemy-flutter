0314ef234b2f0c6c6873066c3217d83f567345dd
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2025-06-28 16:11:55 +0900

    섹션 5: Adding Interactivity, More Widgets & Themeing

---
+++ expense_tracker_app/lib/widgets/expenses-list/expenses_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker_app/models/expense.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(expense.title),
+            SizedBox(height: 4),
+            Row(
+              children: [
+                Text("\$${expense.amount.toStringAsFixed(2)}"),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 6),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
