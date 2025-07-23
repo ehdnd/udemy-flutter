77c3607dc41e60228e56df541e577bab35ab0d1a
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2025-07-23 15:21:38 +0900

    chore: unused import 제거거

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:roll_dice_app/dice_roller.dart';
-import 'package:roll_dice_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
