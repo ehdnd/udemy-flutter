522b067f820301fb2bc3e12e0618ed1a067f56c1
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2025-07-23 14:45:42 +0900

    162

--- meals_app/lib/widgets/category_grid_item.dart
+++ meals_app/lib/widgets/category_grid_item.dart
@@ -2,33 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:meals_app/models/category.dart';
 
-import 'package:meals_app/screens/meals.dart';
-
-import 'package:meals_app/data/dummy_data.dart';
-
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     super.key,
     required this.category,
+    required this.onSelectCategory,
   });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (ctx) => MealsScreen(
-              title: category.title,
-              meals: dummyMeals
-                  .where((x) => x.categories.contains(category.id))
-                  .toList(),
-            ),
-          ),
-        );
-      },
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).colorScheme.primary,
       borderRadius: BorderRadius.circular(16),
       child: Container(
