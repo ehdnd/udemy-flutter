4c85f67d3ea668c8b15f4334a4ffa78900db1d16
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2025-07-26 17:07:18 +0900

    173

---
+++ meals_app/lib/screens/filters.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+/// 유저 인풋 State를 다뤄야하므로 StatefulWidget 사용
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  // 상태가 변화할테니 var로 선언
+  var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _veganFilterSet = false;
+  var _vegetarianFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    // 새로운 화면이므로 `Scaffold` 사용
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            // 새로운 변수를 받아서 상태 변경
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            },
+            // 스위치 옆에 label 표시
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            // 메인 레이블 아래에 표시되는 부가 설명
+            subtitle: Text(
+              'Only include gluten-free meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            // active on 시에 표시될 색상
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            // 스위치 옆에 표시되는 텍스트 영역의 패딩 설정 ??
+            contentPadding: EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
